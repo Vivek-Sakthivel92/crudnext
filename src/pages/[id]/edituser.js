6517b1ec555916a9b7856ffb27f30b88a4ce583c
Vivek-Sakthivel92 <vsakthivel@hexalytics.com>
2023-12-06 11:26:58 +0530

    api url update

--- src/pages/[id]/edituser.js
+++ src/pages/[id]/edituser.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Layout from '@/components/layout/layout';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://loquacious-haupia-d67b64.netlify.app/';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://loquacious-haupia-d67b64.netlify.app';
 
 const Index = ({ user }) => {
   const router = useRouter();
